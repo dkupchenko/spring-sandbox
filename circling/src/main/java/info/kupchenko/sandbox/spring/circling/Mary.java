85de9aa62bcd13b5758298ff57bb6129088b74a3
Dmitry Kupchenko <dmitry@kupchenko.info>
2020-03-08 14:03:39 +0300

    circling-stage-2.0
    Added Mary bean with circular dependency on John. UnsatisfiedDependencyException when DI in constructor.

---
+++ circling/src/main/java/info/kupchenko/sandbox/spring/circling/Mary.java
@@ -0,0 +1,12 @@
+package info.kupchenko.sandbox.spring.circling;
+
+/**
+ * The Mary ...
+ *
+ * @author by Dmitry Kupchenko
+ * @version 1.0
+ * Created on 08.03.2020
+ * Last review on 08.03.2020
+ */
+public class Mary {
+}
